e47067f6c7fd7568811bff0158d8a058ac42f50e
arthit.s <arthit.sekmoh@gmail.com>
2025-12-25 14:16:46 +0700

    update demo7

--- 02_airflow/example/demo7.py
+++ 02_airflow/example/demo7.py
@@ -2,7 +2,8 @@ from airflow import DAG
 from datetime import timedelta
 from airflow.utils.dates import days_ago
 from airflow.operators.python import PythonOperator,BranchPythonOperator
-import random
+from airflow.providers.http.hooks.http import HttpHook
+import json
 
 default_args = {
     'owner': 'mic/iot_team',
@@ -11,125 +12,27 @@ default_args = {
     'retry_delay':timedelta(minutes=0.05),
 }
 
-name = 'toey'
-div = 'mic'
-grade = 'F'
 
-def func1(name):
-    print(f"my name is {name}")
-
-def func2(name,div):
-    print(f"my name is {name} from {div}")
-
-def fun_grade():
-    list_grade = ['a', 'b', 'c', 'f']
-    grade = random.choice(list_grade)
-    return f"task_{grade}"
-
-def a(name,div):
-    print(f"my name is {name} from {div},I wish a !!!")
-
-def b(name,div):
-    print(f"my name is {name} from {div},I wish b !!!")
-
-def c(name,div):
-    print(f"my name is {name} from {div},I wish c !!!")
-
-def f(name,div):
-    print(f"my name is {name} from {div},I wish f !!!")
-
-def push_xcom(**context):
-    task_instance = context['ti']
-    data = 'aaaa'
-    task_instance.xcom_push(key="xcom_aaa", value=data)
-
-def pull_xcom(**context):
-    task_instance = context['ti']
-    xcom_data = task_instance.xcom_pull(task_ids='task_push_xcom', key='xcom_aaa')
-    print(f'pull xcom value->{xcom_data}')
-
+# https://catfact.ninja/fact
+def fetch_data(**kwargs):  
+    http = HttpHook(http_conn_id='api_demo',method='GET')
+    response = http.run(endpoint='fact')
+    data = response.json()
+    print(data)
+    
 with DAG(
     default_args=default_args,
-    dag_id="demo-06",
+    dag_id="demo-07",
     description='this DAG for demo training',
     schedule_interval= "@daily",
     start_date = days_ago(1),
     catchup = False
 
 ) as dag:
-    
-    task1 = PythonOperator(
-        task_id='task1',
-        python_callable=func1,
-        op_kwargs={
-            'name':name,
-        }
+    task_api = PythonOperator(
+        task_id='task_api',
+        python_callable=fetch_data
     )
 
-    task2 = PythonOperator(
-        task_id='task2',
-        python_callable=func2,
-        op_kwargs={
-            'name':name,
-            'div': div
-        }
-    )
-
-    task_a = PythonOperator(
-        task_id='task_a',
-        python_callable=a,
-        op_kwargs={
-            'name':name,
-            'div': div
-        }
-    )
-
-    task_b = PythonOperator(
-        task_id='task_b',
-        python_callable=b,
-        op_kwargs={
-            'name':name,
-            'div': div
-        }
-    )
-    
-    task_c = PythonOperator(
-        task_id='task_c',
-        python_callable=c,
-        op_kwargs={
-            'name':name,
-            'div': div
-        }
-    )
-
-    task_f = PythonOperator(
-        task_id='task_f',
-        python_callable=f,
-        op_kwargs={
-            'name':name,
-            'div': div
-        }
-    )
-
-    select_grade = BranchPythonOperator(
-        task_id='select_grade',
-        python_callable = fun_grade,
-        provide_context = True
-    )
-
-    task_push_xcom = PythonOperator(
-        task_id='task_push_xcom',
-        python_callable=push_xcom,
-        trigger_rule = "one_success"
-    )
-
-    task_pull_xcom = PythonOperator(
-        task_id='task_pull_xcom',
-        python_callable=pull_xcom,
-    )
-    # dag flow
-task1 >> task2 >> select_grade >> [task_a,task_b,task_c,task_f]
-task_a >> task_push_xcom >> task_pull_xcom
-task_b >> task_push_xcom >> task_pull_xcom
-task_c >> task_push_xcom >> task_pull_xcom
-task_f >> task_push_xcom >> task_pull_xcom
+# dag flow
+task_api 
